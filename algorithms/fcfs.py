2ca15d86f8400b1143feeff4ca73816563d22047
Shierwin Clark <shierwinnino@gmail.com>
2026-07-20 23:44:25 +0800

    Update FCFS algorithm

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -7,17 +7,14 @@ def schedule(processes):
 
     for p in processes:
 
-        # CPU idle until process arrives
         while current_time < p.arrival:
             gantt.append("IDLE")
             current_time += 1
 
-        # Response Time
         if not p.started:
             p.started = True
             p.response = current_time - p.arrival
 
-        # Execute completely
         for _ in range(p.burst):
             gantt.append(p.pid)
             current_time += 1
