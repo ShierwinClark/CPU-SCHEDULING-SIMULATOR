c4c30b90359bbac314b34e06d19838541a6c4311
josa10 <josa10@scs.local>
2026-07-20 02:50:54 -0700

    Implement Round Robin scheduling algorithm

---
+++ algorithms/round_robin.py
@@ -0,0 +1,72 @@
+from collections import deque
+
+
+def schedule(processes, quantum):
+
+    processes.sort(key=lambda p: (p.arrival, p.pid))
+
+    queue = deque()
+
+    gantt = []
+
+    current_time = 0
+    completed = 0
+    index = 0
+
+    while completed < len(processes):
+
+        # Add newly arrived processes
+        while (
+            index < len(processes)
+            and processes[index].arrival <= current_time
+        ):
+
+            queue.append(processes[index])
+            index += 1
+
+        # CPU Idle
+        if len(queue) == 0:
+
+            gantt.append("IDLE")
+            current_time += 1
+            continue
+
+        p = queue.popleft()
+
+        if not p.started:
+
+            p.started = True
+            p.response = current_time - p.arrival
+
+        run_time = min(quantum, p.remaining)
+
+        for _ in range(run_time):
+
+            gantt.append(p.pid)
+
+            p.remaining -= 1
+            current_time += 1
+
+            # Check if new process arrived
+            while (
+                index < len(processes)
+                and processes[index].arrival <= current_time
+            ):
+
+                queue.append(processes[index])
+                index += 1
+
+            if p.remaining == 0:
+                break
+
+        # Finished
+        if p.remaining == 0:
+
+            p.completion = current_time
+            completed += 1
+
+        else:
+
+            queue.append(p)
+
+    return gantt
